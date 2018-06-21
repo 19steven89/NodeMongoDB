97d8dfe5aedeabfc44ea4d21539cef0dda9fe160
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-06-21 22:26:17 +0100

    Add GET /todos/:id. added code to handle an http get request from the user which takes in an ObjectId and sends the user back the document from MongoDB. Have tested the http get request using postman.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var {ObjectId} = require("mongodb");
 var {mongoose} = require("./db/mongoose.js");
 var {Todo} = require("./models/todo.js");
 var {User} = require("./models/user.js");
@@ -25,6 +25,34 @@ app.post("/todos", (req, res) => {
   //console.log(req.body);
 });
 
+//GET /Todos.   :id creates id variable on request object.
+//request and response params
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+
+  //ObjectId references the var declared above = to require(mongodb)
+  if(!ObjectId.isValid(id)){
+    console.log("ID not valid");
+    //http 404: Not Found
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      console.log("ID not found");
+      return res.status(404).send();
+    }
+    //send back the Todo from mongodb with the correponding ObjectId sent by the user
+    // in the http request
+    res.send({todo});
+  }, (e) => {
+    //http 400: Bad Request
+    return res.status(400).send();
+  });
+  //res.send(req.params);
+});
+
+
 //get request to get all of the todos
 app.get("/todos", (req, res) => {
   Todo.find().then((todos) => {
@@ -33,7 +61,6 @@ app.get("/todos", (req, res) => {
     res.status(400).send(e);
   });
 });
-
 
 app.listen(3000, () => {
   console.log("Started on Port 3000");
