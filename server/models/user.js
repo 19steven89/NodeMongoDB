eca6ef6aaee7657242ad021e297c1475087afae0
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-06-28 22:35:57 +0100

    added npm validator. added a route for POST /users MongoDb collection, where an email and password can now be set up in the DB

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,34 @@
-var mongoose = require("mongoose");
+const validator = require("validator");
+const mongoose = require("mongoose");
 
 var User = mongoose.model("User", {
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    //verify that the email is unique within MongoDB
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "{value} is not a valid email"
+    }
+  },
+  password: {
+    type: String,
+    minlength: 1,
+    require: true
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = {User};
