3ae4ce52c595ad4e61e8375f9fcbc40f44f427e1
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-07-19 12:17:38 +0100

    upgraded the npm expect using: npm i expect@21.1.0 --save-dev. Then altered a few of the test cases to suit the upgraded expect npm. This includes changing testcases which use toNotExist to toBeFalsy and toExist to toBeTruthy.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -139,7 +139,7 @@ describe("DELETE /todos:id", () => {
       }
 
       Todo.findById(hexId).then((todo) => {
-        expect(todo).toNotExist();
+        expect(todo).toBeFalsy();
         done();
       }).catch((e) => done(e));
     });
@@ -158,7 +158,7 @@ describe("DELETE /todos:id", () => {
       }
 
       Todo.findById(hexId).then((todo) => {
-        expect(todo).toExist();
+        expect(todo).toBeTruthy();
         done();
       }).catch((e) => done(e));
     });
@@ -198,7 +198,7 @@ describe("PATCH /todos/:id", () => {
     .expect((res) => {
       expect(res.body.todo.text).toBe(text);
       expect(res.body.todo.completed).toBe(true);
-      expect(res.body.todo.completedAt).toBeA("number");
+      expect(typeof res.body.todo.completedAt).toBe("number");
     })
     .end(done);
   });
@@ -233,7 +233,7 @@ describe("PATCH /todos/:id", () => {
     .expect((res) => {
       expect(res.body.todo.text).toBe(text);
       expect(res.body.todo.completed).toBe(false)
-      expect(res.body.todo.completedAt).toNotExist();
+      expect(res.body.todo.completedAt).toBeFalsy();
     })
     .end(done);
   });
@@ -273,8 +273,8 @@ describe("POST /users", () => {
     .send({email, pword})
     .expect(200)
     .expect((res) => {
-      expect(res.headers["x-auth"]).toExist()
-      expect(res.body._id).toExist();
+      expect(res.headers["x-auth"]).toBeTruthy()
+      expect(res.body._id).toBeTruthy();
       expect(res.body.email).toBe(email);
     })
     .end((err) => {
@@ -282,9 +282,9 @@ describe("POST /users", () => {
         return done(err);
       }
       User.findOne({email}).then((user) => {
-        expect(user).toExist();
+        expect(user).toBeTruthy();
         //should not be pword above as the user.password should be a hashed value
-        expect(user.password).toNotBe(pword);
+        expect(user.password).not.toBe(pword);
         done();
       }).catch((e) => done(e));
     });
@@ -325,7 +325,7 @@ describe("/users/login", () => {
       })
       .expect(200)
       .expect((res) => {
-        expect(res.headers["x-auth"]).toExist();
+        expect(res.headers["x-auth"]).toBeTruthy();
       })
       .end((err, res) => {
         if(err){
@@ -333,7 +333,7 @@ describe("/users/login", () => {
         }
 
         User.findById(users[1]._id).then((user) => {
-          expect(user.tokens[1]).toInclude({
+          expect(user.toObject().tokens[1]).toMatchObject({
             access: "auth",
             token: res.headers["x-auth"]
           });
@@ -353,7 +353,7 @@ describe("/users/login", () => {
     })
     .expect(400)
     .expect((res) => {
-      expect(res.headers["x-auth"]).toNotExist();
+      expect(res.headers["x-auth"]).toBeFalsy();
     })
     .end((err, res) => {
       if(err){
